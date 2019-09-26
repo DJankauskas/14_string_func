bf1a21cac1e0bc1f7c7e889b9c6c3ad741ae02cf
Darius Jankauskas <d5926j@gmail.com>
2019-09-26 10:44:12 -0400

    Added function headers.

---
+++ function.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_HEADER
+#define FUNCTION_HEADER
+
+int strlen(char *s);
+char *strcpy(char *dest, char *source);
+char *strncat(char *dest, char *source, int n);
+int strcmp(char *s1, char *s2);
+char *strchr(char *s, char c);
+
+#endif
