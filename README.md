91cc9f448d235f426b123813496c764dc3083eca
Darius Jankauskas <d5926j@gmail.com>
2019-10-03 00:31:31 -0400

    Added instructions and credit.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 14_string_func
+
+A C program reimplementing basic C string manipulation functions by Darius Jankauskas and Sunan Tajwar.
+
+To build, run `make`. The resulting executable is named `strtest`.
--- main.c
+++ main.c
@@ -18,43 +18,59 @@ int main()
       "Original target: %s\nOriginal hello: %s\nCopied hello at target: %s\n\n",
       my_readonly_copy,
       readonly,
-      my_readonly_copy
-  );
+      my_readonly_copy);
 
   printf("Testing mystrcmp:\n");
   printf(
-    "Expected val for equal strings: %d.\tMy val: %d.\n", 
-    strcmp(readonly, my_readonly_copy), 
-    mystrcmp(readonly, my_readonly_copy)
-  );
+      "Expected val for equal strings: %d.\tMy val: %d.\n",
+      strcmp(readonly, my_readonly_copy),
+      mystrcmp(readonly, my_readonly_copy));
 
   printf(
-    "Expected val for equal strings: %d.\tMy val: %d.\n", 
-    strcmp(my_readonly_copy, readonly), 
-    mystrcmp(my_readonly_copy, readonly)
-  );
+      "Expected val for equal strings: %d.\tMy val: %d.\n",
+      strcmp(my_readonly_copy, readonly),
+      mystrcmp(my_readonly_copy, readonly));
 
   char *uneq_1 = "ba";
   char *uneq_2 = "bb";
 
   printf(
-    "Expected val for unequal strings (same len): %d.\tMy val: %d.\n", 
-    strcmp(uneq_1, uneq_2), 
-    mystrcmp(uneq_1, uneq_2)
-  );
+      "Expected val for unequal strings (same len): %d.\tMy val: %d.\n",
+      strcmp(uneq_1, uneq_2),
+      mystrcmp(uneq_1, uneq_2));
 
   printf(
-    "Expected val for unequal strings (same len reversed): %d.\tMy val: %d.\n", 
-    strcmp(uneq_2, uneq_1), 
-    mystrcmp(uneq_2, uneq_1)
-  );
+      "Expected val for unequal strings (same len reversed): %d.\tMy val: %d.\n",
+      strcmp(uneq_2, uneq_1),
+      mystrcmp(uneq_2, uneq_1));
 
   char *uneq_3 = "baa";
   printf(
-    "Expected val for unequal strings (different len): %d.\tMy val: %d.\n", 
-    strcmp(uneq_1, uneq_3), 
-    mystrcmp(uneq_1, uneq_3)
-  );
+      "Expected val for unequal strings (different len): %d.\tMy val: %d.\n\n",
+      strcmp(uneq_1, uneq_3),
+      mystrcmp(uneq_1, uneq_3));
+
+  char src[30] = "ThisIsATest";
+  char dest[30] = "ForSystemsLevelProgramming";
+  int x = 10;
+  int y = 15;
+  char *p;
+  p = strncat(dest, src, x);
+  char *q;
+  q = strncat(dest, src, y);
+  printf("Testing strncat:\n");
+  printf("Source string : %s\n", src);
+  printf("Concatenation for %d letters: %s\n\n", x, p);
+
+  printf("Testing strchr:\n");
+  char str[] = "A Test for this Function";
+  char ch = 'h';
+  char cr = 'x';
+  
+  p = mystrchr(str, ch);
+  q = mystrchr(str, cr);
+  printf("String from %c is: %s \n", ch, p);
+  printf("String from %c is: %s \n", cr, q);
 
   return 0;
 }
