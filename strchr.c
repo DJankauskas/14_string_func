f1b7f8ecea3400d1fcec4b0a541bb457112341c8
Darius Jankauskas <d5926j@gmail.com>
2019-10-02 23:40:52 -0400

    Moved to function.c

--- strchr.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-char* mystrchr(char *s, char c){
-    while (*s++ != '\0'){
-        if (*s == c) return s;
-    }
-    return NULL;
-}
--- strlen.c
+++
@@ -1,7 +0,0 @@
-int mystrlen(char *str){
-    int len = 0;
-    for (int i =0; str[i] != 0; i++){
-        len++;
-    }
-    return(len);
-}
